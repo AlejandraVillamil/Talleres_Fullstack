b1977c6d0564fc6a2e912fe3e26d0cd57b902e7b
Alejandra V <eraseunavezpasteleria@gmail.com>
2020-09-26 01:49:47 -0500

    Caclculadora con todas las funciones

--- 20200921_Taller_14_Reto Ánsalo/assets/scripts/calc.js
+++ 20200921_Taller_14_Reto Ánsalo/assets/scripts/calc.js
@@ -1,27 +1,13 @@
 //Declaración de variables
-//Numeros
-let one = document.getElementById('one');
-let two = document.getElementById('two');
-let three = document.getElementById('three');
-let four = document.getElementById('four');
-let five = document.getElementById('five');
-let six = document.getElementById('six');
-let seven = document.getElementById('seven');
-let eight = document.getElementById('eight');
-let nine = document.getElementById('nine');
-let cero = document.getElementById('cero');
-let operator1
-let operator2
-let stgresult
-
-//operaciones
+let operator1 
+let operator2 = 0
+let StgOperation
+let StgOp1
+let StgOp2
+let Stgres
+let operation 
 let result = document.getElementById('result');
-let add = document.getElementById('add');
-let less = document.getElementById('less');
-let mult = document.getElementById('mult');
-let div = document.getElementById('div');
 
-//otros
 let point = document.getElementById('point');
 let reset = document.getElementById('reset');
 let root = document.getElementById('root');
@@ -34,12 +20,135 @@ let del = document.getElementById('del');
 //mensaje
 let msgresult = document.getElementById('msgresult');
 
-
-//Escribir letras en pantalla
+localStorage.setItem('operation',null)
+//Recibir operadores
 document.querySelectorAll('.number').forEach(item => {
     item.addEventListener('click', event => {
         console.log(event.target)
-        let number = event.target.textContent
-        msgresult.innerText += number
+        
+            operator1 = event.target.textContent
+            msgresult.innerText+=operator1
 
-    })})
+           
+        
+  })})
+
+//Recibir operaciones
+        document.querySelectorAll('.operation').forEach(item => {
+            item.addEventListener('click', event => {
+                operation=event.target.textContent
+                localStorage.setItem('operation', operation)
+                localStorage.setItem('operator1', msgresult.innerText)
+                localStorage.setItem('Result', msgresult.innerText)
+                msgresult.innerText=""
+               
+                 })})
+
+// Generar resultado
+result.onclick = function(){
+    
+    localStorage.setItem('operator2', msgresult.innerText)
+
+
+    StgOperation=localStorage.getItem('operation')
+    StgOp1=localStorage.getItem('operator1')
+    StgOp2=localStorage.getItem('operator2')
+    Stgres=localStorage.getItem('Result')
+
+    switch(StgOperation){
+        case "+":
+          operation = parseFloat(StgOp1) + parseFloat(StgOp2)
+          msgresult.innerText=operation
+          localStorage.setItem('Result', operation)
+          
+          break;
+        case "-":
+            operation = parseFloat(StgOp1) - parseFloat(StgOp2)
+            msgresult.innerText=operation
+            localStorage.setItem('Result', operation)
+            
+            break;
+        case "X":
+            operation = parseFloat(StgOp1) * parseFloat(StgOp2)
+            msgresult.innerText=operation
+            localStorage.setItem('Result', operation)
+            
+            break;
+
+        case "÷":
+            operation = parseFloat(StgOp1) / parseFloat(StgOp2)
+            msgresult.innerText=operation
+            localStorage.setItem('Result', operation)
+            
+                  break;
+
+        case "+/-":
+            operation = parseFloat(Stgres) * (-1)
+            msgresult.innerText=operation
+            localStorage.setItem('Result', operation)
+            
+        break;
+
+        case "%":
+            operation = parseFloat(Stgres) / 100
+            msgresult.innerText=operation
+            localStorage.setItem('Result', operation)
+            
+        break;
+
+        case "1/x":
+            operation = 1/ parseFloat(Stgres)
+            msgresult.innerText=operation
+            localStorage.setItem('Result', operation)
+            
+        break;
+        case "x2":
+            operation = parseFloat(Stgres)* parseFloat(Stgres)
+            msgresult.innerText=operation
+            localStorage.setItem('Result', operation)
+            
+        break;
+        case "2√x":
+            operation = Math.sqrt(parseFloat(Stgres)) 
+            msgresult.innerText=operation
+            localStorage.setItem('Result', operation)
+            
+        break;
+        
+        
+
+
+
+
+      }
+
+      localStorage.setItem('operation',null)
+
+
+}
+
+//Borra Todo
+Clear.onclick = function(){
+    localStorage.clear();
+    msgresult.innerText=""
+}
+//CE Borra operador 2
+CE.onclick = function(){
+    localStorage.setItem('operator2', "")
+    msgresult.innerText=""
+}
+//CE Borra una cifra
+del.onclick = function(){
+    msgresult.innerHTML = msgresult.innerText.slice(0, -1)
+    localStorage.setItem('operator2', msgresult.innerText)
+
+}
+
+
+        
+//Generar resultados
+
+/*
+
+ 
+*/
